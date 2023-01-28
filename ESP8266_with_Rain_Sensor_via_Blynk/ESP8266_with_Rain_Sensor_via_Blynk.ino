bfc29f9426e9cb7e65b326c717ffbae4ca20e737
hamza maq <ihamzamaq@gmail.com>
2023-01-28 05:03:39 +0500

    Commiting ESP32 with rain sensor via Blynk ino file and Circuit diagram image.png

---
+++ ESP8266_with_Rain_Sensor_via_Blynk/ESP8266_with_Rain_Sensor_via_Blynk.ino
@@ -0,0 +1,46 @@
+#define BLYNK_TEMPLATE_ID "TMPLgrzkvHG-"
+#define BLYNK_DEVICE_NAME "Rain Sensor"
+#define BLYNK_AUTH_TOKEN "PAj5vA7x1zt9oi7S2k0xnCOVFeyv9CbX"
+
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;//Enter your Auth token
+char ssid[] = "Evo-4G";//Enter your WIFI name
+char pass[] = "sweethome1947";//Enter your WIFI password
+
+BlynkTimer timer;
+bool eventTrigger = false;
+
+void moisture() {
+  int rainSensor = analogRead(A0);
+  rainSensor = map(rainSensor, 0, 1023, 0, 100);
+  Blynk.virtualWrite(V0, rainSensor);
+  Serial.println(rainSensor);
+
+  //if (rainSensor > 30 &&
+     // eventTrigger == false)
+  //{
+   // eventTrigger = true;
+    
+  //  Blynk.logEvent("Tanki is Full", "Gauge is greater than 30");
+ // }
+ // else
+  //{
+  //  eventTrigger = false;
+  //}
+}
+
+void setup() {
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(100L, moisture);
+
+}
+void loop() 
+{
+  Blynk.run();
+  timer.run();
+}
